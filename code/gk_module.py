34805e9f68d4dd32143efb6b88fee21c2ca9212d
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2017-02-27 22:53:43 +0100

    Adding code and tests for module class

--- code/gk_module.py
+++ code/gk_module.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python
 
+import os
+
 GK_SHAPE_TYPES = [
     "box",
     "ellipse",
     "diamond",
-    "folder"
+    "folder",
+    "image"
 ]
 
 
 class GKModule(object):
-    def __init__(self, name=None):
+    def __init__(self, name=None, image=None):
         """contain module definition"""
         self.m_name = name
         self.m_description = None
         self.m_shapetype = GK_SHAPE_TYPES[0]
         self.m_external_link = None
+        if image is not None:
+            self.set_image(image)
 
     def create_node(self, dot_obj):
-        dot_obj.node(self.m_name, shape=self.m_shapetype)
+        """create a node using the specified parameters"""
+        if self.m_shapetype is GK_SHAPE_TYPES[4]:  #image
+            dot_obj.node(
+                name=self.m_name,
+                image=self.m_external_link,
+                labelloc="b",
+                shape="box")
+        else:
+            dot_obj.node(self.m_name, shape=self.m_shapetype)
+
+    def set_image(self, imagepath):
+        """Select an image for the node"""
+        abs_img_path = os.path.abspath(imagepath)
+        if os.path.exists(abs_img_path) is True:
+            self.m_shapetype = GK_SHAPE_TYPES[4]  # image
+            self.m_external_link = os.path.abspath(imagepath)
+            return True
+        else:
+            self.m_shapetype = GK_SHAPE_TYPES[0]
+            self.m_external_link = None
+        return False
 
 
--- test/test_gk_module.py
+++ test/test_gk_module.py
@@ -9,23 +9,55 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", 
 from gk_module import *
 
 
+@pytest.fixture()
+def get_test_data_folder():
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+
+
 def test_graphviz():
     dot = graphviz.Digraph(comment='test_graph')
     dot.node("A")
     dot.node("B")
     dot.node("C")
     dot.edges(['AB', 'BC'])
-    dot.render()
+    dot.render(filename="test_graph1.gv")
 
 
 def test_gkmodule():
     mym1 = GKModule("test1")
     mym2 = GKModule("test2")
 
-    dot = graphviz.Digraph(comment='test_graph2')
+    dot = graphviz.Graph(comment='test_graph2')
     mym1.create_node(dot)
     mym2.create_node(dot)
     dot.edge('test1', 'test2')
-    dot.render()
+    dot.render(filename="test_graph2.gv")
 
+
+def test_gkmodule_init():
+    mym1 = GKModule()
+    assert mym1.m_name is None
+    assert mym1.m_description is None
+    assert mym1.m_shapetype == GK_SHAPE_TYPES[0]
+    assert mym1.m_external_link is None
+
+
+def test_gkmodule_image(get_test_data_folder):
+    mym1 = GKModule("test")
+    assert mym1.set_image("not_existing_picture.png") is False
+
+    img_path = os.path.join(get_test_data_folder, "person-icon.jpg")
+    assert mym1.set_image(img_path) is True
+    assert mym1.m_shapetype == GK_SHAPE_TYPES[4]  # image
+    assert mym1.m_external_link == img_path
+
+    mym2 = GKModule("My second image", img_path)
+    assert mym2.m_external_link == img_path
+
+    # test printing with image
+    dot = graphviz.Graph()
+    mym1.create_node(dot)
+    mym2.create_node(dot)
+    dot.edge(mym1.m_name, mym2.m_name)
+    dot.render(filename="test_img1.gv")
 
