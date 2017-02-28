6ab747231221cfa5fc00b0880fa6d3ad7e1c54c2
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2017-02-28 22:15:52 +0100

    changing project structure and adding __init__.py files

--- test/test_gk_module.py
+++ test/test_gk_module.py
@@ -1,12 +1,11 @@
 #!/urs/bin/python
 
 import os
-import sys
 import pytest
 import graphviz
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "code"))
-from gk_module import *
+from graphlink.core.gk_node import GKNode
+from graphlink.core.gk_node import GK_SHAPE_TYPES
 
 
 @pytest.fixture()
@@ -23,9 +22,9 @@ def test_graphviz():
     dot.render(filename="test_graph1.gv")
 
 
-def test_gkmodule():
-    mym1 = GKModule("test1")
-    mym2 = GKModule("test2")
+def test_gknode():
+    mym1 = GKNode("test1")
+    mym2 = GKNode("test2")
 
     dot = graphviz.Graph(comment='test_graph2')
     mym1.create_node(dot)
@@ -34,16 +33,16 @@ def test_gkmodule():
     dot.render(filename="test_graph2.gv")
 
 
-def test_gkmodule_init():
-    mym1 = GKModule()
+def test_gknode_init():
+    mym1 = GKNode()
     assert mym1.m_name is None
     assert mym1.m_description is None
     assert mym1.m_shapetype == GK_SHAPE_TYPES[0]
     assert mym1.m_external_link is None
 
 
-def test_gkmodule_image(get_test_data_folder):
-    mym1 = GKModule("Chief")
+def test_gknode_image(get_test_data_folder):
+    mym1 = GKNode("Chief")
     assert mym1.set_image("not_existing_picture.png") is False
 
     img_path = os.path.join(get_test_data_folder, "person-icon.jpg")
@@ -51,24 +50,24 @@ def test_gkmodule_image(get_test_data_folder):
     assert mym1.m_shapetype == GK_SHAPE_TYPES[4]  # image
     assert mym1.m_external_link == img_path
 
-    mym3 = GKModule("John", img_path)
+    mym3 = GKNode("John", img_path)
     assert mym3.m_external_link == img_path
 
-    mym2 = GKModule("Bob", img_path)
+    mym2 = GKNode("Bob", img_path)
     assert mym2.m_external_link == img_path
-    mym4 = GKModule("+41791234567")
+    mym4 = GKNode("+41791234567")
 
     # test printing with image
     dot = graphviz.Graph()
-    
+
     mym1.create_node(dot)
     mym2.create_node(dot)
     mym3.create_node(dot)
-    
+
     dot.edge(mym1.m_name, mym2.m_name)
     dot.edge(mym1.m_name, mym3.m_name)
     dot.edge(mym3.m_name, mym4.m_name)
     dot.edge(mym4.m_name, mym2.m_name)
-    
+
     dot.render(filename="test_img1.gv")
 
